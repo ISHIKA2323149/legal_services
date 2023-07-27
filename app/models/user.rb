595cfe43c220994337cadebf25dcc57476df6729
Ishika <ishika.upadhyay@preciousinfosystem.com>
2023-07-27 13:48:02 +0530

    added active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_one_attached :image
 
   validates_uniqueness_of :name
   scope :all_except, ->(user) { where.not(id: user) }
